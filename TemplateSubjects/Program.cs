28223fb4057dada5c2e6114d975422e71902d849
Robert <rbalderrama41@gmail.com>
2019-10-01 18:07:37 -0400

    inicial

---
+++ TemplateSubjects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateSubjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = UtilPfd.GetData(@"C:\Users\Robert\Desktop\Usip\6to Semestre\TS I\TS I\Classroom\Horarios Sistemas II-2019.pdf");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TemplateSubjects/UtilPfd.cs
@@ -0,0 +1,64 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TemplateSubjects
+{
+    public static class UtilPfd
+    {
+        public static string GetData(string pdfFileName)
+        {
+            StringBuilder result = new StringBuilder();
+            PdfReader reader = new PdfReader(pdfFileName);
+            string pageText;
+            for (int page = 1; page <= reader.NumberOfPages; page++)
+            {
+                pageText = PdfTextExtractor.GetTextFromPage(reader, page);
+                result.Append(UseText(pageText));
+            }
+            reader.Close();
+            return result.ToString();
+        }
+
+        public static string UseText(string page)
+        {
+            StringBuilder Line = new StringBuilder();
+            string[] splittedText = page.Split('\n');
+            string text;
+            foreach (string textLine in splittedText)
+            {
+                if (textLine != "" && textLine != "(*)")
+                {
+                    char secondChar = textLine.ToCharArray().ElementAt(1);
+                    char thirdChar = textLine.ToCharArray().ElementAt(2);
+                    if (char.IsNumber(secondChar) && char.IsNumber(thirdChar))
+                    {
+                        text = textLine + '\n';
+                        Line.Append(text);
+                    }
+                }
+                
+            }
+            return Line.ToString();
+        }
+
+        public static void TextToModel(string text)
+        {
+            Regex rx = new Regex(@"\b(?<word>\w+)\s+(\k<word>)\b", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+            MatchCollection matches = rx.Matches(text);
+
+            for (int ctr = 0; ctr < matches.Count; ctr++)
+            {
+                Console.WriteLine("{0} repeated at positions and",
+                                  matches[ctr].Value);
+                Console.ReadKey();
+            }
+        }
+    }
+}
